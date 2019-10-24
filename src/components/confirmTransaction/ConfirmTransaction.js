9f5c6d73f8c111830ffd535fb57ce21111ffc9e0
Pablo <strigoiholyknight45@gmail.com>
2019-10-23 21:29:30 -0300

    pija

---
+++ src/components/confirmTransaction/ConfirmTransaction.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ConfirmTransaction extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+ConfirmTransaction.propTypes = {
+
+}; 
+
+export default ConfirmTransaction;
