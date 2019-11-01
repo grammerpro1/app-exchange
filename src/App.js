grammerpro1/app-exchange

commit 07221c71ff1724458584b0a7c8e33be50de13751

Axios post and other shits

src/App.js
@@ -6,7 +6,7 @@ import Signup from './components/generics/signup/Signup';
 import Home from './components/generics/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Offer from './components/offer/Offer';
-import ConfirmTransaction from './components/confirmTransaction/ConfirmTransaction';
+import ConfirmTransaction from './components/ConfirmTransaction/ConfirmTransaction';
 
 class App extends Component {
     constructor(props) {

src/api/Methods.js
@@ -1,1 +1,15 @@
+import Axios from 'axios';
+
 export const url = 'http://localhost:3001/transactions';
+
+const doApiPost = (transaction) => {
+    console.log("doApiPost");
+    console.log(transaction);
+    Axios.post(url, transaction)
+    .then(response => {
+        console.log(response);
+        console.log(response.data);
+    });
+}
+
+export default doApiPost;

src/components/offer/Offer.js
@@ -3,6 +3,8 @@ import { Container, Row, Col, Label, Input, Jumbotron, Button } from 'reactstrap
 import NavBar from '../generics/navbar/NavBar';
 import './styles.css';
 import swal from 'sweetalert';
+import doApiPost from './../../api/Methods';
+import TransactionCard from '../transactionCard/TransactionCard';
 
 class Offer extends Component {
     constructor(props) {
@@ -10,51 +12,66 @@ class Offer extends Component {
 
         this.state = {};
     }
-    
+
     handleClickCalculate(event) {
         event.preventDefault();
-        console.log(this.state);
-        if(
+        if (
             (this.amountInput.value === '' || this.amountInput.value === null) ||
             (this.fromCurrencyInput.value === '' || this.fromCurrencyInput.value === null) ||
             (this.toCurrencyInput.value === '' || this.toCurrencyInput.value === null) ||
             (this.erateInput.value === '' || this.erateInput.value === null)
         ) {
-            swal("Error", "Todos los campos del formulario deben de estar completos", "error"); 
+            swal("Error", "Todos los campos del formulario deben de estar completos", "error");
         } else {
-           console.log("amountInput = " + this.amountInput.value);
-           console.log("fromCurrencyInput = " + this.fromCurrencyInput.value);
-           console.log("toCurrencyInput = " + this.toCurrencyInput.value);
-           console.log("erateInput = " + this.erateInput.value);
-
             this.setState({
-                    from: this.fromCurrencyInput.value, 
-                    to: this.toCurrencyInput.value,
-                    erate: this.erateInput.value,
-                    amount: this.amountInput.value
-              });
+                from: this.fromCurrencyInput.value,
+                to: this.toCurrencyInput.value,
+                erate: parseFloat(this.erateInput.value),
+                amount: parseFloat(this.amountInput.value)
+            });
         }
     }
+
+    handleClickPublish(event) {
+        event.preventDefault();
+        swal({
+            title: "¿Seguro desea publicar la oferta?",
+            // text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                    doApiPost({...this.state, state: 0, id: 1010, offerer: "phaller"});
+                    swal("Poof! Your imaginary file has been deleted!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
+    }
+
 
     render() {
         let { from, to, amount, erate } = this.state;
         let hasData = false;
-        let conversion = amount * erate; 
+        let conversion = amount * erate;
 
-        if(from != null && to != null && amount != null && erate != null) {
+        if (from != null && to != null && amount != null && erate != null) {
             hasData = true;
-            conversion = amount * erate; 
+            conversion = amount * erate;
         }
 
         return (
             <div>
-                <NavBar/>
+                <NavBar />
                 <div className="offer-layout">
                     <Container>
                         <Row>
                             <Col>
                                 <Label>Monto</Label>
-                                <Input type="number" innerRef={amountInput => this.amountInput = amountInput}/>
+                                <Input type="number" innerRef={amountInput => this.amountInput = amountInput} />
                             </Col>
                             <Col>
                                 <Label>Moneda Origen</Label>
@@ -64,7 +81,7 @@ class Offer extends Component {
                             </Col>
                             <Col>
                                 <Label>Moneda Cambio</Label>
-                                <Input type="select"  innerRef={toCurrencyInput => this.toCurrencyInput = toCurrencyInput}>
+                                <Input type="select" innerRef={toCurrencyInput => this.toCurrencyInput = toCurrencyInput}>
                                     <option>USD</option>
                                     <option>CHL</option>
                                     <option>VLF</option>
@@ -73,7 +90,7 @@ class Offer extends Component {
                             </Col>
                             <Col>
                                 <Label>Tasa de cambio</Label>
-                                <Input type="number" innerRef={erateInput => this.erateInput = erateInput}/>
+                                <Input type="number" innerRef={erateInput => this.erateInput = erateInput} />
                             </Col>
                             <Col>
                                 <Button className="buttonCalc" color="primary" size="lg" onClick={this.handleClickCalculate.bind(this)}>Calcular</Button>
@@ -81,14 +98,15 @@ class Offer extends Component {
                         </Row>
                         <Row>
                             <Col>
-                                <br/>
+                                <br />
                                 {
-                                    hasData && 
+                                    hasData &&
                                     <Jumbotron>
                                         <h1>${amount} {from} -> ${conversion} {to}</h1>
-                                        <hr/>
+                                        <hr />
                                         <p>Se ha convertido el monto en base a una tasa de cambio de {erate}</p>
                                         <p>¿Desea publicar la oferta?</p>
+                                        <Button color="primary" block onClick={this.handleClickPublish.bind(this)}>Publicar</Button>
                                     </Jumbotron>
                                 }
                             </Col>

commit 48f544b0d19803348fe68d17405c05806c77cab3

Fixed bug introduced by Pablo in 07221c71ff1724458584b0a7c8e33be50de13751.

src/App.js
@@ -6,7 +6,7 @@ import Signup from './components/generics/signup/Signup';
 import Home from './components/generics/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Offer from './components/offer/Offer';
-import ConfirmTransaction from './components/ConfirmTransaction/ConfirmTransaction';
+import ConfirmTransaction from './components/confirmTransaction/ConfirmTransaction';
 
 class App extends Component {
     constructor(props) {