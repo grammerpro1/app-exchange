c497f5a1b34f745f4ddc7a87efb546ded7ffa438
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-13 00:04:39 -0300

    First approach at linking login to API.

--- src/components/generics/login/Login.js
+++ src/components/generics/login/Login.js
@@ -1,49 +1,105 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Form, FormGroup, Label, Input, Container, Jumbotron, Button } from 'reactstrap';
-import './styles.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Jumbotron,
+  Button
+} from "reactstrap";
+import "./styles.css";
+import swal from "sweetalert";
+import axios from "axios";
+
+const baseUrl = "https://topicos.azurewebsites.net/api/";
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onClick = this.onClick.bind(this);
-    }
+    this.onInputKeyPress = this.onInputKeyPress.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
 
-    onClick(event) {
-        event.preventDefault();
-        if(this.userNameInput.value === 'phaller' && this.passwordInput.value === '1234') {
-            console.log(this);
-            //Se debería obtener el userId de alguna forma. Capaz que el login tendria que ser por cedula y que ese sea el Id.
-            console.log(`${this.userNameInput.value}, ${this.passwordInput.value}, todo bien`);
-            this.props.history.push('/home', {userId: 19640328, username: this.userNameInput.value});
-        } else {
-            console.log(`${this.userNameInput.value}, ${this.passwordInput.value}, todo mal, el Apa se la come`);
-        }      
+  onInputKeyPress(event) {
+    if (event.key === "Enter") {
+      this.onRegisterClick(event);
     }
+  }
 
-    render() {
-        return (
-            <div>
-                <Container className="centered">
-                    <div className="centered login-layout">
-                        <h1 className="display-3">¡Bienvenido!</h1>
-                        <Form>
-                            <FormGroup>
-                                <Label for="userName">Nombre de usuario</Label>
-                                <Input id="userName" placeholder="Ingrese aquí su nombre de usuario" innerRef={userNameInput => this.userNameInput = userNameInput}/>
-                                <Label for="userName">Contraseña</Label>
-                                <Input id="password" type="password" placeholder="Ingrese aquí su contraseña" innerRef={passwordInput => this.passwordInput = passwordInput}/>
-                            </FormGroup>
-                        </Form>
-                        <p className="lead">
-                            <Button size="lg" block onClick={this.onClick}>Iniciar sesión</Button>
-                        </p>
-                    </div>
-                </Container>
-            </div>
-        );
+  onLoginClick(event) {
+    event.preventDefault();
+    if (
+      this.userNameInput.value === "" ||
+      this.emailInput.value === "" ||
+      this.passwordInput.value === ""
+    ) {
+      swal(
+        "Error",
+        "Todos los campos del formulario deben de estar completos.",
+        "error"
+      );
+    } else {
+      console.log(`Intentando ingresar a usuario ${this.userNameInput.value}`);
+      axios
+        .post(baseUrl + "Sessions", {
+          params: {
+            user_name: this.userNameInput.value,
+            pass: this.passwordInput.value
+          }
+        })
+        .then(response => {
+          console.log(response);
+          console.log(response.data);
+          // const userId = 12345678;
+          // this.props.history.push("/home", {
+          //   userId: userId,
+          //   username: this.userNameInput.value
+          // });
+        })
+        .catch(error => {
+          console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+          console.log(error);
+          swal("", "no", "error");
+        });
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <Container className="centered">
+          <div className="centered login-layout">
+            <h1 className="display-3">¡Bienvenido!</h1>
+            <Form>
+              <FormGroup>
+                <Label for="userName">Nombre de usuario</Label>
+                <Input
+                  id="userName"
+                  placeholder="Ingrese aquí su nombre de usuario"
+                  innerRef={input => (this.userNameInput = input)}
+                />
+                <Label for="userName">Contraseña</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Ingrese aquí su contraseña"
+                  innerRef={input => (this.passwordInput = input)}
+                />
+              </FormGroup>
+            </Form>
+            <p className="lead">
+              <Button size="lg" block onClick={this.onLoginClick}>
+                Iniciar sesión
+              </Button>
+            </p>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Login;
