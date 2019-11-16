ff9bfebe5bb28fdb2008a70a50615a665bb18db1
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-16 17:15:21 -0300

    Added signup logic.

--- src/api/Methods.js
+++ src/api/Methods.js
@@ -5,8 +5,8 @@ const baseUrl = "https://topicos.azurewebsites.net/api/";
 function doApiGet(suburl, params) {
     return Axios.get(baseUrl + suburl, params);
 }
-// function doApiPost(suburl, params) {
-//     return Axios.post(baseUrl + suburl, params);
-// }
+function doApiPost(suburl, params) {
+    return Axios.post(baseUrl + suburl, params);
+}
 
-export default { doApiGet };
+export default { baseUrl, doApiGet, doApiPost };
