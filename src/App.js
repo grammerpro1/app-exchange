1d3823d181a8297730697b224f23ba80a9f71c5e
Rodrigo Castro <castro.rodrigo22@gmail.com>
2019-11-19 19:28:11 -0300

    ruta con mayusculas fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/generics/home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Offer from "./components/offer/Offer";
 import CalificarPublicaciones from "./components/calificarPublicacion/CalificarPublicaciones";
-import ConfirmTransaction from "./components/ConfirmTransaction/ConfirmTransaction";
+import ConfirmTransaction from "./components/confirmTransaction/ConfirmTransaction";
 
 class App extends Component {
     constructor(props) {
